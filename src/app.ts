c1bb03740e1e4bb1c60fde9d5b1063a4e5182798
Muhammed shifin <muhammedshifin424@gmail.com>
2023-08-19 02:05:28 +0530

    updated the cors setup

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:5173','passcrafter-x.netlify.app', "*"]
+    origin: ['http://localhost:5173','https://passcrafter-x.netlify.app', "*"]
 
   })
 );
